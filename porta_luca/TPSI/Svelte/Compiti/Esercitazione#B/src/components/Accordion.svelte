8d9f88390e0c5daf55fd7b6997034e79cc6520b0
Luca-Porta <luca.porta@istitutoagnelli.it>
2023-11-10 10:08:07 +0100

    Compito #B

---
+++ porta_luca/TPSI/Svelte/Compiti/Esercitazione#B/src/components/Accordion.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Elemento from "./Elemento.svelte";
+    let id=1;
+</script>
+
+<main>
+    <div class="contenitore">
+        <Elemento id={id}/>
+        <Elemento id={++id}/>
+    </div>
+</main>
+
+<style>
+    main{
+        margin: auto;
+        border: 5px solid blue;
+        width: 80%;
+        height: 80%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .contenitore{
+        width: 80%;
+        height: 80%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
