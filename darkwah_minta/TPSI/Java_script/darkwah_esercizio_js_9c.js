ae67d49f50f8f30e5bbeea26ca01f922f795c86c
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2023-12-21 16:12:37 +0100

    aggiunta es.9c

---
+++ darkwah_minta/TPSI/Java_script/darkwah_esercizio_js_9c.js
@@ -0,0 +1,22 @@
+let Utente = function (marca, modello, anno, n_propet, tipo) {
+    (this.marca = marca),
+    (this.modello = modello),
+    (this.anno = anno),
+    (this.n_propet = n_propet),
+    (this.tipo = tipo),
+    (this.prod = function () {
+        console.log(`>> Auto ${this.marca} ${this.modello} dell'${this.anno} di ${this.n_propet} tipologia ${this.tipo} verrà venduto in Italia`);
+    }),
+    (this.bloc = function () {
+        console.log(`>> Auto ${this.marca} ${this.modello} dell'${this.anno} di ${this.n_propet} tipologia ${this.tipo} verrà bloccato in India`);
+    })
+};
+
+
+let utente1 = new Utente("Lamborghini", "fg45","1990","Luca","elettrica");
+let utente2 = new Utente("Tesla", "Indiana","2020","Marco","elettrica");
+let utente3 = new Utente("Stellantis", "Roma","2021","Samanta","ibrida");
+
+utente1.prod();
+utente2.bloc();
+utente3.prod();
