a1861964f0da7a170bde1aea73420d3a15e5c329
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-12-21 15:22:34 +0100

    add es09a

--- scattaglia_alessandro/TPSI/JS/scattaglia_esercizio_js_09.js
+++ scattaglia_alessandro/TPSI/JS/scattaglia_esercizio_js_09.js
@@ -1,9 +1,9 @@
 let user = prompt("USERNAME");
 let password;
 user || alert("Canceled");
-user == null || user == "Admin" || alert("Idky");
+user == null || user == false || user == "Admin" || alert("Idky");
 
 user == "Admin" && (password = prompt("PASSWORD"));
 password || alert("Canceled");
-password == null || password == "Agnell1no" || alert("Wrong password");
+password == null || password == false || password == "Agnell1no" || alert("Wrong password");
 password == "Agnell1no" && alert("Welcome");
