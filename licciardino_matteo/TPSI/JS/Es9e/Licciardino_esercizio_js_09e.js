7ae6e404bfe31e21ee0417cf228106bcbe329fde
user <matteo.licciardino@istitutoagnelli.it>
2024-01-19 12:37:58 +0100

    Fare gli es da e alla m

---
+++ licciardino_matteo/TPSI/JS/Es9e/Licciardino_esercizio_js_09e.js
@@ -0,0 +1,21 @@
+let orario = {
+    Ora: 12,
+    Minuti: 10,
+    Secondi: 19
+};
+
+let orario2 = {
+    Ora: 14,
+    Minuti: 12,
+    Secondi: 21
+};
+
+const horas = () => {
+    
+}
+
+const main = () => {
+    
+}
+
+main()
