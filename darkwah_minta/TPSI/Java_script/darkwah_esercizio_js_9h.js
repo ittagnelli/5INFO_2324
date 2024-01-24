7d71014cc298f6dd6002647c43dfa02029e45e01
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2024-01-24 13:08:41 +0100

    aggiunta es.eh

---
+++ darkwah_minta/TPSI/Java_script/darkwah_esercizio_js_9h.js
@@ -0,0 +1,9 @@
+let a = 13
+let b = 22
+
+function maggiore(prima, seconda)
+{
+    return prima > seconda? "LA PRIMA LETTERA E' PIU' GRANDE": "LA SECONDA LETTERA E' PIU' GRANDE"
+}
+
+console.log(maggiore(a, b))
