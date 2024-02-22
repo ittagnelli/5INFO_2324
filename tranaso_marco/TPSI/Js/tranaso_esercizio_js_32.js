cb3358f5421888f8f5a77cc6bc2358a0327ff504
Marco-Tranaso <marco.tranaso@istitutoagnelli.it>
2024-02-22 19:19:44 +0100

    es js 31-40

---
+++ tranaso_marco/TPSI/Js/tranaso_esercizio_js_32.js
@@ -0,0 +1,13 @@
+function anagramma(array){
+let map=new Map();
+
+for(let word of arr)
+{
+
+    let sorted=word.toLowerCase().split("").sort().join('');
+}
+
+}
+let array=['omar', 'sette', 'roma', 'teste', 'mora', 'colazione', 'locazione'];
+
+anagramma(array);
