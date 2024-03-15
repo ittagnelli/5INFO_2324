eac904650840b9deee0ab14c30b2ef30a1422018
MarcoCellini <marco.cellini@istitutoagnelli.it>
2024-03-15 21:59:40 +0100

    cellini: add ex 2 and 3 form in sveltekit

---
+++ cellini_marco/TPSI/Svelte/back-end/Esercitazione_03/src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let data;
+    export let form;
+    let nome, 
+        cognome,
+        eta,
+        error = false;
+
+    if (form?.form_error) {
+        error = true;
+        nome = form.form_vals.nome;
+        cognome = form.form_vals.cognome;
+        eta = form.form_vals.eta;
+    }
+
+    console.log(form);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<form method="POST">
+    <div><label for="nome">Nome</label></div>
+    <div>
+        <input type="text" name="nome" id="nome" size="40" bind:value={nome} />
+        <p class={error && nome?.length == 0 ? '' : 'hidden'}>Nome invalido</p>
+    </div>
+    <div><label for="cognome">Cognome</label></div>
+    <div>
+        <input type="text" name="cognome" id="cognome" size="40" bind:value={cognome}/>
+        <p class={error && cognome?.length == 0 ? '' : 'hidden'}>Cognome invalido</p>
+    </div>
+    <div><label for="eta">Età</label></div>
+    <div>
+        <input type="number" name="eta" id="eta" size="15" bind:value={eta}/>
+        <p class={error && eta?.length == 0 ? '' : 'hidden'}>Età invalida</p>
+    </div>
+    <div><input type="submit" /></div>
+</form>
+
+<ul>
+    {#each data.utenti as utente}
+        <li>{utente.nome} {utente.cognome} ha {utente.eta}</li>
+    {/each}
+</ul>
+
+<style>
+    p {
+        color: red;
+    }
+    
+    .hidden {
+        display: none;
+    }
+</style>
