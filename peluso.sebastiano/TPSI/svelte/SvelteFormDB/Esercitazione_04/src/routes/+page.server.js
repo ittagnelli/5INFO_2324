3b3b6ec965e96fb0458dd9cacd842827f9560664
SebastianoPeluso <sebastiano.peluso@istitutoagnelli.it>
2024-04-02 23:00:41 +0200

    add esercitazioni da 4 a 8

---
+++ peluso.sebastiano/TPSI/svelte/SvelteFormDB/Esercitazione_04/src/routes/+page.server.js
@@ -0,0 +1,49 @@
+
+
+import Database from 'better-sqlite3';
+
+const db = new Database('USER.db');
+
+
+
+let utenti = [
+    {nome: 'mario', cognome:'rossi', eta:18},
+    {nome: 'luca', cognome:'bianchi', eta:14},
+    {nome: 'gianni', cognome:'verdi', eta:16},
+    {nome: 'pino', cognome:'silvedtre', eta:19},
+];
+
+export function load({params}){
+    console.log("ESECUZIONE FUNZIONE LOAD: ", Date.now());
+
+
+    return {utenti};
+    
+}
+
+export const actions = {
+    default: async({cookies, request}) =>{
+        const data = await request.formData();
+        console.log("I VALORI del FORM sono", data);
+
+        const user = {
+            nome: data.get("nome"),
+            cognome: data.get("cognome"),
+            eta: data.get("eta")
+        }
+
+        
+        if (user.nome && user.cognome && user.eta) {
+            utenti.push(user);
+        }else{
+            return{
+                form_error: true,
+                form_vals: user
+                
+            }
+        }
+        
+
+    },
+};
+
