a8ae1568999dda2c0d7c115b17dcaac1ffd313ed
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2024-05-06 10:53:07 +0200

    add todo-api

--- scattaglia_alessandro/TPSI/todo-api/src/routes/api/todos/[[id]]/+server.js
+++ scattaglia_alessandro/TPSI/todo-api/src/routes/api/todos/[[id]]/+server.js
@@ -1,4 +1,8 @@
-import { json } from "@sveltejs/kit"
+import { json } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
+import Database, { Database } from "better-sqlite3";
+
+const db = new Database("TODO.db", { verbose: console.log });
 
 let todos = [
     {
@@ -14,75 +18,176 @@ export async function GET({ params, request, url }) {
 
     console.log("ricevuto HTTP GET con parametro:", params);
 
+    const sql_azione2 = db.prepare("SELECT * FROM todo");
+    const sql_azione3 = db.prepare("SELECT * FROM todo WHERE id= ?");
+    const sql_azione4 = db.prepare("SELECT * FROM todo WHERE done= ?");
+    const sql_azione5 = db.prepare("SELECT * FROM todo WHERE priority= ?");
 
-    if (params.id) {
-        const todo = todos.filter(t => t.id == params.id)[0];
-        return json(todo);
+    const exec_query = (azione, param) => {
+        const todo = param || param == 0 ? azione.all(param) : azione.all();
+        if (todo.length > 0) {
+            return json(todo, { status: 200 });
+        }
+        else {
+            return json({}, { status: 404 });
+        }
+    };
+    try {
+        if (params.id) {
+            return exec_query(sql_azione3, params.id)
+        }
+        if (url.searchParams.has("priority")) {
+            return exec_query(sql_azione5, +urlSearchParams.get("priority"));
+        }
+
+        if (url.searchParams.has("done")) {
+            return exec_query(sql_azione4, +JSON.parse(urlSearchParams.get("done")));
+        }
+
+        return exec_query(sql_azione2);
+    } catch (e) {
+        return json({}, { status: 500 });
 
     }
-    else {
-        let res = todos;
-        if (url.searchParams.has('priority')) {
-            res = todos.filter(t => t.priority == +url.searchParams.get('priority'));
 
-        }
-        else if (url.searchParams.has('done')) {
-            res = todos.filter(t => t.done == (url.searchParams.get('done') === 'true'));
-        }
-        return json(res);
-    }
+
 
 }
 
 
 export async function POST({ request }) {
-    console.log("Ricevuto HTTP POST");
-    const body = await request.json();
-    console.log("POST BODY:", body);
-    body['id'] = Math.ceil(Math.random() * 100);
-    todos.push(body);
+    try {
+        const body = await request.json();
+        console.log("ricevuto HTTP POST con body:", body);
 
-    return json('OK');
+        const sql_azione1 = db.prepare("INSERT INTO TODO (task,done,priority) VALUES (@task,@done,@priority");
+
+        const res = sql_azione1.run({
+            task: body.task,
+            done: +body.done,
+            priority: +body.priority,
+        });
+        if (res.changes == 1) {
+            body["id"] = res.lastInsertRowid;
+            return json(body, {
+                status: 201,
+                headers: new headers({ "Location": `http://localhost:5173/api/todos/${body["id"]}` })
+            });
+        }
+
+    }
+    catch (e) {
+        console.log(e);
+        return json({}, {
+            status: 500
+        });
+    }
+
 }
 
 
 export async function PUT({ params, request }) {
-    console.log("Ricevuto HTTP PUT con parametro:", params);
+    try {
+        const body = await request.json();
+        console.log("ricevuto  HTTP PUT con parametro: ", params);
+        console.log("PUT BODY", body);
 
-    const body = await request.json();
-    console.log("PUT BODY:", body);
+        const sql_azione6 = db.prepare("UPDATE todo SET task = @task, done = @done, priority= @priority WHERE id= @id");
+        const res = sql_azione6.run({
+            id: +params.id,
+            task: body.task,
+            done: +body.done,
+            priority: +body.priority
 
-    const todoIndex = todos.findIndex(t => t.id == params.id);
-    if (todoIndex !== -1) {
-        todos[todoIndex] = { ...todos[todoIndex], ...body };
-        return json({ message: 'OK' });
-    } else {
-        return json({ error: 'Todo non trovato' }, { status: 404 });
+        });
+        console.log(res);
+        if (res.changes == 0) {
+            return json({}, { status: 404 });
+        }
+        else if (res.changes == 1) {
+            return json({}, { status: 200 });
+        }
+
+    }
+    catch (e) {
+        console.log(e)
+        return json({}, { status: 500 });
     }
 }
 
 
 
 export async function PATCH({ params, request }) {
-    console.log("Ricevuto HTTP PATCH con parametro: ", params);
+    try {
+        const body = await request.json();
+        console.log("Ricevuto HTTP PATCH con parametro : ", params);
+        console.log("PUT BODY:", body);
 
-    let body = await request.json();
 
-    let todo = todos.findIndex(t => t.id == params.id);
-    const key = Object.keys(body);
-    todos[todos][key] = body[key];
+        const sql_azione7 = db.prepare("UPDATE todo SET task = @task WHERE id = @id");
+        const sql_azione8 = db.prepare("UPDATE todo SET priority = @priority WHERE id = @id");
+        const sql_azione9 = db.prepare("UPDATE todo SET done = @done WHERE id = @id");
+        const sql_get_todo = db.prepare("SELECT * FROM todo WHERE id= ?");
 
-    return json('OK');
+        let key = Object.keys(body)[0];
+        let res;
+        switch (key) {
+            case 'task':
+                res = sql_azione7.run({
+                    id: +params.id,
+                    task: body.task
+                });
+                break;
+            case 'priority':
+                res = sql_azione8.run({
+                    id: +params.id,
+                    priority: +body.priority
+                })
+                break;
+            case 'done':
+                res = sql_azione9.run({
+                    id: +params.id,
+                    done: body.done
+                })
+                break;
+            default:
+                return json({}, { status: 500 });
+        }
+
+        if (res.changes === 0) {
+            return json({}, { status: 404 });
+
+        }
+        else if (res.changes === 1) {
+            const todo = sql_get_todo.all(+params.id);
+            return json(todo, { status: 200 });
+
+        }
+    } catch (e) {
+        console.log(e);
+        return json({}, { status: 500 });
+    }
 }
 
 
 export async function DELETE({ params, request }) {
-    console.log("Ricevuto HTTP DELETE con parametro: ", params);
+    try {
+        console.log("Ricevuto HTTP DELETE con parametro: ", params);
 
-    todos = todos.filter(t => t.id != params.id);
-    return json('OK');
+        const sql_azione10 = db.prepare("DELETE FROM todo WHERE id = @id");
+        const res = sql_azione10.run({
+            id: +params.id
+        });
 
-
+        if (res.changes === 0) {
+            return json({}, { status: 404 });
+        }
+        else if (res.changes === 1) {
+            return new Response(null, { status: 204 });
+        }
+    } catch (e) {
+        return json({}, { status: 500 });
+    }
 }
 
 
